76a2e2d3d330068dd1a1aab207c1db35f4d556d9
ldmarcos <ldmarcos@outlook.com>
2022-08-27 22:49:58 +0000

    Apache v1.0

---
+++ iac-apache.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Iniciando"
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+cd /temp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+echo "Finalizado..."
